57df36021891ac6f6e8679be052191927e7a4e05
ilfate <ilfate@gmail.com>
2012-12-28 02:22:00 +0400

    sync

--- app/tpl/GameMap/index.tpl
+++ app/tpl/GameMap/index.tpl
@@ -2,3 +2,8 @@
 <canvas id="demoCanvas" width="500" height="300">
     alternate content
 </canvas>
+
+<a onclick="CanvasActions.map.addY(-1)" > /\ </a>
+<a onclick="CanvasActions.map.addY(1)" > \/ </a>
+<a onclick="CanvasActions.map.addX(-1)" > <-- </a>
+<a onclick="CanvasActions.map.addX(1)" > --> </a>
--- public/js/canvasActions.js
+++ public/js/canvasActions.js
@@ -17,34 +17,33 @@ CanvasActions = function() {
   this.init = function()
   {
     this.stage = new createjs.Stage("demoCanvas");
-	this.container = new createjs.Container();
-	this.width = $('#demoCanvas').width();
-	this.height = $('#demoCanvas').height();
-	
-	var manifest = this.getMapManifest();
+    this.container = new createjs.Container();
+    this.width = $('#demoCanvas').width();
+    this.height = $('#demoCanvas').height();
+
+    var manifest = this.getMapManifest();
     this.loader.loadManifest(manifest);
-	
+  
     this.loader.onFileLoad = function(event){CanvasActions.handleFileLoad(event)};
     this.loader.onComplete = function(){CanvasActions.afterLoad()};
-	
-	
+  
+  
   }
   
   this.afterLoad = function()
   {
-	this.createMap();  
-	  
-	this.container.x = this.width/2;  
-	this.container.y = this.height/2;  
-	this.stage.addChild(this.container);  
+    this.createMap();  
+
+    this.stage.addChild(this.container);  
     this.stage.update(); 
     createjs.Ticker.addListener(CanvasActions);
     createjs.Ticker.setFPS(30); 
     createjs.Ticker.useRAF = true;
   }
   
-  this.getMapManifest = function() {
-	return [
+  this.getMapManifest = function() 
+  {
+    return [
       {src:"/images/game/tile1.png",id:"floor"},
       {src:"/images/game/tile1_damaged.png",id:"floor_d"},
       {src:"/images/game/tile2.png",id:"empty"},
@@ -54,31 +53,16 @@ CanvasActions = function() {
   
   this.createMap = function() 
   {
+    var map_container = new createjs.Container();
+    map_container.x = this.width/2;  
+    map_container.y = this.height/2;  
+    this.map = new IL.Map(map_container);
+    this.map.draw();
     
-	
-    
-    var map = new IL.Map();
-	cells = ["floor","wall","floor_d"];
-	for(var x = -5; x < 5; x++)
-	{
-		for(var y = -5; y < 5; y++)
-		{
-			var rand = Math.floor(Math.random() * 3);
-			map.addSimpleCell(x, y, cells[rand]);
-		}
-	}
-//    map.addSimpleCell(0, 0, 'floor');
-//    map.addSimpleCell(1, 0, 'wall');
-//    map.addSimpleCell(1, 1, 'wall');
-//    map.addSimpleCell(0, 1, 'wall');
-//    map.addSimpleCell(-1, 1, 'wall');
-//    map.addSimpleCell(-1, 0, 'wall');
-//    map.addSimpleCell(-1, -1, 'wall');
-//    map.addSimpleCell(0, -1, 'wall');
-//    map.addSimpleCell(1, -1, 'wall');
-    
-	map.draw(this.container);
-    
+    this.addTick(function(elapsedTime){
+      CanvasActions.map.draw();
+    });
+    this.container.addChild(map_container);
    // var square = new createjs.Shape();
    // square.graphics.beginFill("black").drawRoundRect(0,0,30,80,0);
     
@@ -149,7 +133,7 @@ CanvasActions = function() {
   }
   this.handleFileLoad = function(event) {
     //CanvasActions.assets.push(event);
-	this.addObject(event.result, event.id);
+  this.addObject(event.result, event.id);
   }
 }
 
@@ -161,11 +145,25 @@ function IL () {
 }
 IL = new IL();
 
-IL.Map = function() 
+IL.Map = function(container) 
 {
+  this.container = container;
+  this.needDraw = true;
   this.cells = [];
   this.cell_idx = [];
   this.cell_width = 32;
+  this.map_radius = 3;
+  this.x = 0;
+  this.y = 0;
+  this.Point = new IL.Point(0, 0);
+  this.addX = function(val) {
+    this.Point.x += val;
+    this.needDraw = true;
+  }
+  this.addY = function(val) {
+    this.Point.y += val;
+    this.needDraw = true;
+  }
   
   this.addCell = function(Cell)
   {
@@ -186,14 +184,63 @@ IL.Map = function()
       return false;
     }
   }
+  this.daleteCell = function(x, y) {
+    var name = x + '_' + y;
+    var idx = $.inArray(name, this.cell_idx)
+    if(idx != -1) {
+      //this.cells[idx] = '';
+      this.cell_idx[idx] = '';
+    }
+  }
+  this.loadCell = function(x, y)
+  {
+    var cells = ["floor","wall","floor_d"];
+    var rand = Math.floor(Math.random() * 3);
+    this.addSimpleCell(x, y, cells[rand]);
+    return this.getCell(x, y);
+  }
+  
   this.getMiddlePoint = function()
   {
-    return new ILPoint(0,0);
+    return this.Point;
   }
-  this.draw = function(container) {
-	  for(var i in this.cells) {
-		  this.cells[i].draw(container, this.cell_width);
-	  }
+  this.checkAllVisibleCells = function()
+  {
+    var middle = this.getMiddlePoint();
+    var window_x = -this.map_radius;
+    var window_y = -this.map_radius;
+    for(var x = (middle.x - this.map_radius); x <= (middle.x + this.map_radius); x++,window_x++)
+    {
+      window_y = -this.map_radius;
+      for(var y = (middle.y - this.map_radius); y <= (middle.y + this.map_radius); y++,window_y++)
+      {
+        var cell = this.getCell(x, y);
+        if(!cell) {
+          cell = this.loadCell(x, y);
+        }
+        cell.set(window_x, window_y);
+        
+      }
+    }
+  }
+  this.draw = function() 
+  {
+    if(this.needDraw) 
+    {
+      this.checkAllVisibleCells();
+      this.container.removeAllChildren();
+      for(var i in this.cells) 
+      {
+        
+        if(this.cells[i].visible) 
+        {
+          
+          this.cells[i].draw(this.container, this.cell_width);
+          this.cells[i].visible = false;
+        }
+      }
+      this.needDraw = false;
+    }
   }
   
 }
@@ -213,6 +260,9 @@ IL.Point = function(x, y)
 
 IL.Cell = function(Point, type)
 {
+  this.visible = false;
+  this.shape = new createjs.Shape();
+  this.window_point = new IL.Point(0, 0);
   if(Point) 
   {
     this.Point = Point;
@@ -221,11 +271,43 @@ IL.Cell = function(Point, type)
   }
   this.type = type ? type : 'empty';
   
-  this.draw = function(container, width) {
-	  var cell = new createjs.Shape();
-	  cell.graphics
-		.beginBitmapFill(CanvasActions.getObject(this.type))
-		.drawRect(width * this.Point.x, width * this.Point.y, width, width);
-	  container.addChild(cell);
+  this.set = function(x, y)
+  {
+    this.window_point.x = x;
+    this.window_point.y = y;
+    this.visible = true;
+  }
+  
+  this.draw = function(container, width) 
+  {
+    this.shape.graphics
+      .clear()
+      .beginBitmapFill(CanvasActions.getObject(this.type))
+      .drawRect(width * this.window_point.x, width * this.window_point.y, width, width);
+    container.addChild(this.shape);
   }
 }
+
+$(document).keypress(function(event) {
+  info(event);
+  switch(event.keyCode)
+  {
+    case 37:
+      CanvasActions.map.addX(-1);
+      break;
+    case 38:
+      CanvasActions.map.addY(-1);
+      break;
+    case 39:
+      CanvasActions.map.addX(1);
+      break;
+    case 40:
+      CanvasActions.map.addY(1);
+      break;
+  }
+});
+
+//$(document).bind("keydown", "up", function() { CanvasActions.map.addY(-1) });
+//$(document).bind("keydown", "down", function() { CanvasActions.map.addY(1) });
+//$(document).bind("keydown", "left", function() { CanvasActions.map.addX(1) });
+//$(document).bind("keydown", "right", function() { CanvasActions.map.addY(-1) });
